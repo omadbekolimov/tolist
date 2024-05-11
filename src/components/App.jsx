a51e6753f1c447a472e7bd0c33cc2d116828477a
omadbekolimov <omadbekolimov96@gmail.com>
2024-05-11 09:54:52 +0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+const App = () => {
+  return (
+    <>
+      <div className="container mt-3">
+        <form>
+          <div className="row">
+            <div className="col-3">
+              <label htmlFor="" className="form-label ">
+                Name :
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your Name :"
+              />
+            </div>
+            <div className="col-3">
+            <label htmlFor="" className="form-label ">
+                Name :
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your Name :"
+              />
+            </div>
+            <div className="col-3">
+            <label htmlFor="" className="form-label ">
+                Name :
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your Name :"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+export default App;
